0cb82558c7caeb361b6260384ca6329259a6920c
Vijender Singh Bajwa <vbajwamd@gmail.com>
2016-03-21 15:18:49 -0400

    working on controllers, now on delete

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,4 +4,41 @@ class PatientsController < ApplicationController
     @patients = Patient.all
   end
 
+  def new
+    @patient = Patient.new
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def create
+    @patient = Patient.create(patient_params)
+
+    redirect_to @patient
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+    @patient.update(params)
+
+    redirect_to @patient
+  end
+
+  def destroy
+    @patient=  Patient.find(params[:id])
+    @patient.destroy
+
+    redirect_to patients_path
+  end
+
+  private
+  def patient_params
+  params.require(:patient).permit(:name, :mrn, :age, :sex, :insurance, :smoker, :surgery)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'patients#index'
+
+  resources :patients
+
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
